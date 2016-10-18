1535e8d95c3be44959b2e238f8bb8ef6178fd4c3
federf <fede_franco_93@hotmail.com>
2016-10-18 15:25:50 -0300

    vehicle implementation

--- redes/Vehicle.java
+++ redes/Vehicle.java
@@ -1,6 +1,29 @@
 package redes;
 
 public class Vehicle {
-	int reserved=0;
-	static final int seats=30;
+	static int reserved = 0;
+	static final int seats = 30;
+	int id;
+
+	public static int available() {
+		return seats - reserved;
+	}
+
+	public static boolean reserve(int n) {
+		if (n <= available()) {
+			System.out.println("Reservaste " + n);
+			reserved += n;
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean cancel(int n) {
+		if (n <= reserved) {
+			System.out.println("Cancelaste " + n);
+			reserved -= n;
+			return true;
+		}
+		return false;
+	}
 }
