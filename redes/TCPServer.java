983b972613cb0f35d732fa0bfc864ad0e804d319
Diego <mattanadiego@gmail.com>
2016-10-18 16:24:40 -0300

    cambios en los servidores

--- redes/TCPServer.java
+++ redes/TCPServer.java
@@ -1,9 +1,57 @@
 package redes;
 
-public class TCPServer {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public TCPServer() {
-		// TODO Auto-generated constructor stub
-	}
+public class TCPServer extends Thread {
 
+    String clientSentence;
+    String capitalizedSentenceTCP;
+    ServerSocket welcomeSocket;
+    DataOutputStream outToClient;  
+       
+    public TCPServer() throws IOException {
+        welcomeSocket = new ServerSocket(1234);
+    }
+    
+    /*Metodo que se encarga de procesar un request del cliente*/
+    /*private void request(String data)throws IOException{
+        String[] arrayData = data.split(" ");
+        String command = arrayData[0];
+        Main.parameter = -1; 
+        Main.command = command;
+        Terminal.time++;
+        Message message = new Message(Terminal.time,Main.pid); //agregarle el parametro al mensaje
+        if(arrayData.length > 1){
+            Main.parameter = Integer.parseInt(arrayData[1]);
+        }
+        Main.q.add(message);
+        UDPServer.broadcast(message, Main.REQUEST);
+    }*/
+    
+    @Override
+    public void run() {
+        System.out.println("TCP SERVER ON");
+ 
+            Socket connectionSocket;
+            try {
+                connectionSocket = welcomeSocket.accept();
+                while (!connectionSocket.isClosed()) {                                      
+                    BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                    outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                    clientSentence = inFromClient.readLine();
+                    //request(clientSentence);
+                }
+                
+            } catch (IOException ex) {
+                Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        
+    }
 }
--- redes/UDPServer.java
+++ redes/UDPServer.java
@@ -1,9 +1,138 @@
 package redes;
 
-public class UDPServer {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-	public UDPServer() {
-		// TODO Auto-generated constructor stub
-	}
+public class UDPServer extends Thread {
 
+    static int replyCount = 0;
+    DatagramSocket serverSocket;
+    byte[] receiveData;
+    byte[] sendData;
+
+    public UDPServer() throws SocketException {
+        serverSocket = new DatagramSocket(1235);
+        receiveData = new byte[1024];
+        sendData = new byte[1024];
+    }
+
+    @Override
+    public void run() {
+        System.out.println("UDP SERVER ON");
+        while (true) {
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            try {
+                serverSocket.receive(receivePacket);
+                String sentence = new String(receivePacket.getData());
+                String[] s = sentence.split("-");
+                //Message msg = new Message(Integer.parseInt(s[1]),Integer.parseInt(s[3])); //s[1] = tiempo , s[3]= pid
+                /*switch(s[0]){ //s[0] = comando
+                    case Main.REQUEST:    
+                         // crea el mensaje nuevo con lo que le llego
+                        Terminal.time = Math.max(Terminal.time, msg.getTime()) + 1;              
+                        Main.q.add(msg);
+                        reply(msg.getPid());
+                    break;
+                    case Main.REPLY:
+                        Terminal.time = Math.max(Terminal.time, msg.getTime()) + 1;
+                        replyCount++;
+                        if(replyCount >= Main.peerData.size()){  
+                            checkAndExecute();
+                        }
+                        
+                    break;
+                    case Main.RELEASE:
+                        Main.q.remove();
+                        Terminal.time = Math.max(Terminal.time, msg.getTime()) + 1;
+                        Terminal.reserved = Integer.parseInt(s[2]); // s[2] = estado
+                        if(replyCount >= Main.peerData.size()){  
+                            checkAndExecute(); //se fija si es su turno y ejecuta 
+                        }                   
+                    break;
+                
+                }*/
+
+                InetAddress IPAddress = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                String capitalizedSentence = sentence.toUpperCase();
+                sendData = capitalizedSentence.getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+            } catch (IOException ex) {
+                Logger.getLogger(UDPServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+    
+    /*Metodo que ejecuta el codigo correspondiente al request*/
+    public static void exec(){        
+        /*switch(Main.command){
+            case "available":
+                System.out.println("Quedan " + Terminal.available() + " lugares");
+            break;
+            case "reserve":
+                Terminal.reserve(Main.parameter);
+            break;
+            case "cancel":
+                Terminal.cancel(Main.parameter);
+            break;
+        }*/
+    }
+    
+    /*Metodo que llama a exec solo si tiene derecho a acceder a la region critica*/
+    private void checkAndExecute() throws IOException{
+        /*if (!Main.q.isEmpty() && Main.q.peek().getPid() == Main.pid) {
+            Main.q.remove(); 
+            exec();
+            release();
+        }*/
+    }
+    
+    /*Metodo que manda release a todos*/
+    public static void release() throws IOException {
+        /*replyCount = 0;
+        Terminal.time++;
+        Message m = new Message(Terminal.time,Main.pid,Terminal.reserved);
+        broadcast(m,Main.RELEASE);*/
+    }
+    
+    /*Metodo que manda reply a quien corresponda*/
+    private void reply(int dst) throws IOException {
+        /*int i;
+        for (i = 0; !(Main.peerData.get(i).getPid() == dst);i++){};
+        Terminal.time++;
+        Message m = new Message(Terminal.time,Main.pid);
+        DatagramSocket clientSocket = new DatagramSocket();            
+        InetAddress IPAddress = InetAddress.getByName(Main.peerData.get(i).getIp());
+        String sentence = Main.REPLY+ "-" + m.toString();
+        byte[] sendData = new byte[1024];
+        sendData = sentence.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, Main.peerData.get(i).getUdpPort());
+        clientSocket.send(sendPacket);             
+        clientSocket.close();*/  
+    }
+    
+    /*Metodo que manda un mensaje a todos los peers*/
+    /*public static void broadcast(Message message, String type) throws IOException{
+        for (int i = 0; i < Main.peerData.size(); i++) {
+            DatagramSocket clientSocket = new DatagramSocket();            
+            InetAddress IPAddress = InetAddress.getByName(Main.peerData.get(i).getIp());
+            String sentence = "";
+            if(message != null)
+                sentence = type+ "-" + message.toString();
+            else
+                sentence = type+ "-";
+            byte[] sendData = new byte[1024];
+            sendData = sentence.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, Main.peerData.get(i).getUdpPort());
+            clientSocket.send(sendPacket);                
+            clientSocket.close();  
+        }
+    }*/
 }
